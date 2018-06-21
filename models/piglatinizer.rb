c51cebf429154377c74b6d71eac3a52b27c46022
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 02:11:45 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,6 +13,7 @@ class PigLatinizer
       first_letter = moldable.split("").shift
       puts moldable
       first_to_last = word.split("").push(first_letter)
+      first_to_last.shift
       puts "#{first_to_last} - "
     }
     latinized.join("")
