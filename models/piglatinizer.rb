2396ba370cbb1a50df88c62cffd097acaa67e999
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 02:06:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
   def pig_latinize
     array = @text.split(" ")
     latinized = array.map { |word|
-      puts "#{word.split("").unshift(word.split("").pop)} - "
+      puts "#{word.split("").push(word.split("").shift)} - "
     }
     latinized.join("")
     #binding.pry
