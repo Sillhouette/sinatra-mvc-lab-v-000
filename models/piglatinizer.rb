2d8e9f3bfb5a8e5fae79fe3f599ded96170157a6
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 01:39:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,13 @@
 class PigLatinizer
+  attr_reader :text
 
+  initialize(text)
+    @text = text
+  end
+
+  def pig_latinize
+    array = @text.to_a
+    last_to_first = array.unshift(array.pop)
+    "#{last_to_first}ay"
+  end
 end
