02f13b08dada3fffcebf897ce852c5f9431a2802
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 02:07:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,7 @@ class PigLatinizer
     latinized = array.map { |word|
       first_letter = word.split("").shift
       first_to_last = word.split("").push(first_letter)
-      puts first_to_last
+      puts "#{first_to_last} - "
     }
     latinized.join("")
     #binding.pry
