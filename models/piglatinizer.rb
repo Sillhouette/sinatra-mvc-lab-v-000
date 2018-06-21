e35e6e9059c9cab3c04ff8d8babc98394f3f1833
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 04:03:00 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinizer
       "#{first_to_last.join("")}ay"
     }
     changed_array = latinized.to_s
-    puts changed_array
+    puts changed_array[0]
 
     #binding.pry
     #{}"#{last_to_first}ay"
