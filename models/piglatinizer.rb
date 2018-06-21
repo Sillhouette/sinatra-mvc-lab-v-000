46de2a1a4442a3bf67fd5cb5a89bb169dcb58e5d
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 02:11:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,8 +9,9 @@ class PigLatinizer
   def pig_latinize
     array = @text.split(" ")
     latinized = array.map { |word|
-      first_letter = word.split("").shift
-      puts word
+      moldable = word
+      first_letter = moldable.split("").shift
+      puts moldable
       first_to_last = word.split("").push(first_letter)
       puts "#{first_to_last} - "
     }
